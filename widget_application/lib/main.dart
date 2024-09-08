a3efab815f3c2c7c5fc35c924c8c2da7278e083f
Ikem Samuel <ikemcharlseanthony@gmail.com>
2024-09-08 08:14:36 -0700

    first commit

---
+++ widget_application/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(   
+      appBar: AppBar(
+      title: const Text('Row in Flutter'),
+    ),
+    body: const Row(  
+    mainAxisAlignment:
+    MainAxisAlignment.spaceEvenly,
+    children:[  
+      Icon(Icons.star, color: Colors.black),
+      Icon(Icons.star, color: Colors.black),
+      Icon(Icons.star, color: Colors.black),
+    ],  
+    ),
+    ),
+    );
+  }
+}
